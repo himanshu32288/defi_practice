9d402f6606b9bda88777aec5619b22a0ef4e4048
Himanshu <himanshu32288@gmail.com>
2022-05-29 17:29:04 +0530

    migrate bug

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -9,11 +9,11 @@ contract Migrations {
     modifier restricted(){
         if(msg.sender==owner) _;
     }
-    function set_completed(uint completed) public restricted{
+    function setCompleted(uint completed) public restricted{
         last_migrated=completed;
     } 
     function upgrade(address new_address) public restricted{
         Migrations upgraded=Migrations(new_address);
-        upgraded.set_completed(last_migrated);
+        upgraded.setCompleted(last_migrated);
     }
 }
